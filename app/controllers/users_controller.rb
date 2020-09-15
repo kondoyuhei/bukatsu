9a4601a908f63f6845baa4679119fb2b1e6e3b69
kondoyuhei <gyomu@kondo-yuhei.com>
2020-09-16 00:08:05 +0900

    ログイン・セッションの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
-  before_action :set_new_user, only: [:login, :new]
+  before_action :set_new_user, only: [:login_page, :new]
 
   def login_page
   end
 
   def login
     @user = User.find_by(name: login_params[:name])
-    # binding.pry
     if @user && @user.authenticate(login_params[:password])
-      session[:user_id] == @user.id
+      session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to root_path
     else
@@ -36,7 +35,7 @@ class UsersController < ApplicationController
   end
 
   def login_params
-    params.permit(:name, :password, :authenticity_token, :commit)
+    params.require(:user).permit(:name, :password, :authenticity_token, :commit)
   end
 
 end
