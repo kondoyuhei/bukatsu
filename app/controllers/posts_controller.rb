e1d40a35bee98e4ec0694cdb6a1cf5193b807451
kondoyuhei <gyomu@kondo-yuhei.com>
2020-09-15 19:07:26 +0900

    ユーザー認証機能の設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,23 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user, except: [:index]
+
   def index
     @posts = Post.all.order(created_at: :DESC)
   end
 
   def show
     @post = Post.find_by(post_id: params[:id])
+  end
+
+  def new
+    @post = Post.find_by(post_id: params[:id])
+  end
+
+  private
+
+  def authenticate_user
+    unless @authenticate_user
+      redirect_to root_path
+    end
   end
 end
