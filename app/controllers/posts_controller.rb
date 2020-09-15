f3b7089d883fe3840a85c76cd1ea0b2b5d19082e
kondoyuhei <gyomu@kondo-yuhei.com>
2020-09-15 16:04:17 +0900

    ビュー・スタイルシート骨格部分の仮置

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(created_at: :DESC)
+  end
+
+  def show
+    @post = Post.find_by(post_id: params[:id])
   end
 end
