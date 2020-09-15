c0af19b784c1c573a562eeed336b3f6fa64b9ac5
kondoyuhei <gyomu@kondo-yuhei.com>
2020-09-15 21:03:34 +0900

    ログイン機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   before_action :set_new_user, only: [:login, :new]
 
+  def login_page
+  end
+
   def login
+    @user = User.find_by(name: login_params[:name])
+    # binding.pry
+    if @user && @user.authenticate(login_params[:password])
+      session[:user_id] == @user.id
+      flash[:notice] = "ログインしました"
+      redirect_to root_path
+    else
+      flash[:notice] = "正しく入力してください"
+      render 'users/login_page'
+    end
   end
 
   def new
@@ -20,6 +33,10 @@ class UsersController < ApplicationController
 
   def set_new_user
     @user = User.new
+  end
+
+  def login_params
+    params.permit(:name, :password, :authenticity_token, :commit)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'posts#index'
-  get '/login',  to:'users#login'
-  get '/logout', to:'users#logout'
+  root   'posts#index'
+  get    '/login',  to: 'users#login_page'
+  post   '/login',  to: 'users#login'
+  patch  '/login',  to: 'users#login'
+  get    '/logout', to: 'users#logout'
   resources :posts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
