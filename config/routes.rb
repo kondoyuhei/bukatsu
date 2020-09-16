282c92045edeeaa79d4e3903b233a0e5b73873fe
kondoyuhei <gyomu@kondo-yuhei.com>
2020-09-17 01:13:57 +0900

    ユーザー登録ページの修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   delete '/logout', to: 'users#logout'
   get    '/users/list', to: 'users#list'
   get    '/posts/list', to: 'posts#list'
+  get    '/students/:id/new', to: 'students#new'
   resources :posts
   resources :users
-  resources :students, except: :index
+  resources :students, except: [:index, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
