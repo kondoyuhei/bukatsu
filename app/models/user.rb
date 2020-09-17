5f9d261eddab9c39ace65f9cb20c052c4fb9c3d0
kondoyuhei <gyomu@kondo-yuhei.com>
2020-09-17 20:46:21 +0900

    生徒名検索メソッドの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,21 +13,44 @@ class User < ApplicationRecord
     validates :admin            # ユーザー種類
   end
 
+  # ****************************************
+  #   ユーザーが管理者であるか判定するメソッド
+  # ****************************************
   def administrator
     return true if admin == 11
 
     false
   end
 
+  # ****************************************
+  #   ユーザーが生徒であるか判定するメソッド
+  # ****************************************
   def student
     return true if admin == 1
 
     false
   end
 
+  # ****************************************
+  #   ユーザーが保護者であるか判定するメソッド
+  # ****************************************
   def parent
     return true if admin == 2
 
     false
   end
+
+
+  # ****************************************
+  #   保護者に対して生徒を返すメソッド
+  # ****************************************
+  def get_student
+    if StudentDetail.find_by(parent1: self.id)
+      student_id = StudentDetail.find_by(parent1: self.id).user_id
+    elsif StudentDetail.find_by(parent2: self.id)
+      student_id = StudentDetail.find_by(parent2: self.id).user_id
+    end
+    return User.find_by(id: student_id) if student_id
+  end
+
 end
