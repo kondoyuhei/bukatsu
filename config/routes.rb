46951b6a8e59847796a5e640e2966a11411d76db
kondoyuhei <gyomu@kondo-yuhei.com>
2020-09-17 17:37:37 +0900

    ルーティングの修正
    ネスト・member・collectionの使用

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,23 @@ Rails.application.routes.draw do
   delete '/logout', to: 'users#logout'
   get    '/users/list', to: 'users#list'
   get    '/posts/list', to: 'posts#list'
-  get    '/students/:id/new', to: 'students#new'
-  get    '/users/first_user', to: 'users#first_user'
-  post   '/users/first_user', to: 'users#first_user_registration'
+  # get    '/users/first_user', to: 'users#first_user'
+  # post   '/users/first_user', to: 'users#first_user_registration'
   resources :posts
   resources :users
-  resources :students
+  resources :students, except: [:new, :create] do
+    # member do 以下に指定したルーティングに「method :xyz」を指定すると、
+    #   medthod 'students/:id', to: 'students#xyz'
+    # が追加される
+    member do
+      get  :set
+      post :add
+    end
+    collection do
+      get  :list
+      get  :first_user
+      post :first_user_registration
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
