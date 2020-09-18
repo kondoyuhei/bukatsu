bfea551a6ff1903a290e246ee09ad57de0d5c4bc
kondoyuhei <gyomu@kondo-yuhei.com>
2020-09-19 00:49:04 +0900

    post-formのCSS作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,16 +10,40 @@ class PostsController < ApplicationController
   def show
   end
 
+  # ****************************************
+  #   投稿の新規作成
+  # ****************************************
   def new
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'お知らせを投稿しました'
+      redirect_to posts_path
+    else
+      flash[:notice] = '投稿できませんでした'
+      render '/posts/index'
+    end
+  end
+
+  # ****************************************
+  #   投稿の編集
+  # ****************************************
   def edit
+  end
+
+  def update
   end
 
   private
 
   def set_post
-    @post = Post.find_by(post_id: params[:id])
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content).merge(user_id: @current_user.id)
   end
 end
